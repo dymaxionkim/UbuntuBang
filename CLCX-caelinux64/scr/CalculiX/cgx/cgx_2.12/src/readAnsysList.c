ea9600811ee08abc6cbadbce3bad1cf4bc6fda39
dymaxionkim <dymaxion.kim@gmail.com>
2019-06-18 18:23:37 +0900

    Layout

--- CLCX-caelinux64/scr/CalculiX/cgx/cgx_2.12/src/readAnsysList.c
+++
@@ -1,7 +0,0 @@
-#include <cgx.h>
-
-int readAnsysList(char *datin, Summen *apre, Sets **sptr, Nodes **nptr, Elements **eptr, Datasets **lptr  )
-{
-  printf("not implemented in the internet version\n");
-  return(-1);
-}
