fbd2a28f3a0e0f185d49124cc74f6683ba491298
dymaxionkim <dymaxion.kim@gmail.com>
2019-07-09 11:57:25 +0900

    Add dependency

--- i3-gaps.sh
+++ i3-gaps.sh
@@ -8,7 +8,7 @@ sudo systemctl enable lightdm
 
 ###########################################################
 # Build i3-gaps
-sudo apt -y install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+sudo apt -y install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev automake
  
 cd $HOME/git
  
