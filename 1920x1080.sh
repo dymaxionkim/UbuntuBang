0cbfd9c20a4771b3acb9a4fe95d1e6e3cf348614
osboxes <dymaxion.kim@gmail.com>
2020-05-12 12:20:25 +0900

    Fix modename

--- 1920x1080.sh
+++ 1920x1080.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cvt 1920 1080
-xrandr --addmode Virtual1 1920x1080_60.00
-xrandr --output Virtual1 --mode 1920x1080_60.00
-
+xrandr --newmode "1920x1080_60" 173.00 1920 2040 2248 2576 1080 1081 1084 1118 -hsync +vsync
+xrandr --addmode Virtual1 1920x1080_60
+xrandr --output Virtual1 --mode 1920x1080_60
